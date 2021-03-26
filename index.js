f46824dd4876e27307f7310f35c72c74716d9866
Uhevela <66133903+Uhev-png@users.noreply.github.com>
2021-03-26 12:41:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const { token } = require('./config.json');
+
+const { readdirSync } = require('fs');
+
+const { join } = require('path');
+
+client.commands= new Discord.Collection();
+
+const prefix = ';';
+//You can change the prefix if you like. It doesn't have to be !
+
+
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
